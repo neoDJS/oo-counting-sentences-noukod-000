62d836e74800eae4083dd8e975d04a9a8efbe86a
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-03 19:30:19 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,7 +3,7 @@ require 'pry'
 class String
 
   def sentence?
-    self.end_with?("!")
+    self.end_with?(".")
   end
 
   def question?
@@ -11,7 +11,7 @@ class String
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
